e8e30b9bdafd44a724eee43f127f8d7bf3f5ec70
horizontti <77754465+horizontti@users.noreply.github.com>
2021-06-23 14:08:59 +0300

    Create fxmanifest.lua

---
+++ e_ac/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'common'
+
+client_scripts {
+    'core/conffi.lua',
+    'core/client.lua',
+}
+server_scripts { 
+    'core/conffi.lua',
+    'core/server.lua',
+}
+
+shared_scripts {
+    'core/shared.lua',
+}
